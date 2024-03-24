0ae52904f53c92165d28bf9bc8fbda28fcd247f3
Clebson Santos <clebsonsantosmtt@gmail.com>
2024-03-24 09:10:14 -0300

    chore: adicionando cpnj no footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2023 Clebson Santos<a href="/" className="hover:underline"></a>
+          © 2023 Clebson Santos<a href="/" className="hover:underline"></a> <strong>CNPJ</strong>: 41.413.640/0001-00
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
           <a href="https://github.com/clebsonsantos" rel="noreferrer" target="_blank">
